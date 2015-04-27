72de2f3e4665261b73363bc863e5da50c58a1fb2
Anton Korotkov <antonkorotkoff@gmail.com>
2015-04-27 17:49:15 +0300

    wip

---
+++ lib/classes/class-bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ */
+
+namespace wpCloud\Docker {
+
+    if ( !class_exists( 'wpCloud\Docker\Bootstrap' ) ) {
+
+        /**
+         * Class Bootstrap
+         * @package wpCloud\Docker
+         */
+        final class Bootstrap {
+
+            /**
+             *
+             */
+            function __construct() {
+
+            }
+        }
+    }
+
+}
